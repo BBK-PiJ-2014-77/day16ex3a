789e11751a5f4ceb72592181320832fbc9458618
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2015-02-02 20:10:07 +0000

    Initial commit

---
+++ src/cat1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+/**
+ * Created by digibrose on 02/02/2015.
+ */
+public class cat1 {
+
+    public static void main (String[] args ){
+
+        File filein = new File(args[0]);
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filein));
+            String line;
+            while ((line = in.readLine()) != null){
+                System.out.println(line);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("file does not exist");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
