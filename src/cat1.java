282ce080c5af5dc5168c2843a7a0b19b132c561f
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2015-02-02 20:18:49 +0000

    commit with multiple arguments

--- src/cat1.java
+++ src/cat1.java
@@ -7,18 +7,20 @@ public class cat1 {
 
     public static void main (String[] args ){
 
-        File filein = new File(args[0]);
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filein));
-            String line;
-            while ((line = in.readLine()) != null){
-                System.out.println(line);
+        for (int i = 0; i < args.length; i++) {
+            File filein = new File(args[i]);
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(filein));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.println(line);
+                }
+                in.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("file does not exist");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            in.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("file does not exist");
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
     }
